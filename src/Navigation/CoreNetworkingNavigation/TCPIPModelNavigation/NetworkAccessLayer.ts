7c74a8994fc94ca9ae92293c91753eb45215c48c
Justin <j.hebenstreit25@gmail.com>
2025-03-26 11:30:37 -0500

    updated TCPIP Physical Layer navigation

--- src/Navigation/CoreNetworkingNavigation/TCPIPModelNavigation/NetworkAccessLayer.ts
+++ src/Navigation/CoreNetworkingNavigation/TCPIPModelNavigation/NetworkAccessLayer.ts
@@ -6,18 +6,47 @@ const NetworkAccessLayer: Subpage = {
     {
       name: "Layer 2: Data Link",
       subpages: [
-        { name: "Overview", path: "/corenetworking/tcpipmodel/networkaccesslayer/overview" },
-        { name: "Burned-In Address (BIA)", path: "/corenetworking/tcpipmodel/networkaccesslayer/bia" },
-        { name: "Address Resolution Protocol (ARP)", path: "/corenetworking/tcpipmodel/networkaccesslayer/arp" },
+        {
+          name: "Overview",
+          path: "/corenetworking/tcpipmodel/networkaccesslayer/overview"
+        },
+        {
+          name: "Burned-In Address (BIA)",
+          path: "/corenetworking/tcpipmodel/networkaccesslayer/bia"
+        },
+        {
+          name: "Address Resolution Protocol (ARP)",
+          path: "/corenetworking/tcpipmodel/networkaccesslayer/arp"
+        },
       ],
     },
     {
       name: "Layer 1: Physical",
       subpages: [
-        { name: "Overview", path: "/corenetworking/tcpipmodel/networkaccesslayer/overview" },
-        { name: "Data Representation", path: "/corenetworking/tcpipmodel/networkaccesslayer/datarepresentation" },
-        { name: "Communication Standards", path: "/corenetworking/tcpipmodel/networkaccesslayer/standardsofcommunication" },
-        { name: "Encapsulation/De-encapsulation", path: "/corenetworking/tcpipmodel/networkaccesslayer/encapsulationanddeencapsulation" },
+        {
+          name: "Overview",
+          path: "/corenetworking/tcpipmodel/networkaccesslayer/overview"
+        },
+        {
+          name: "Data Representation",
+          path: "/corenetworking/tcpipmodel/networkaccesslayer/datarepresentation"
+        },
+        {
+          name: "Communication Standards",
+          path: "/corenetworking/tcpipmodel/networkaccesslayer/standardsofcommunication"
+        },
+        {
+          name: "Encapsulation/De-encapsulation",
+          path: "/corenetworking/tcpipmodel/networkaccesslayer/encapsulationanddeencapsulation"
+        },
+        {
+          name: "The Bit and Digital Signals",
+          path: "/corenetworking/tcpipmodel/networkaccesslayer/bitanddigitalsignals"
+        },
+        {
+          name: "Data Transmission Methods",
+          path: "/corenetworking/tcpipmodel/networkaccesslayer/datatransmissionmethods"
+        },
       ],
     },
   ],
