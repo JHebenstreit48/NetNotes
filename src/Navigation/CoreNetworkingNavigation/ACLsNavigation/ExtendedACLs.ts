811d278310f77fcf4ca8229314519e6c67104a64
Justin <j.hebenstreit25@gmail.com>
2025-03-26 00:09:46 -0500

    fixed some errors in ACL navigation

--- src/Navigation/CoreNetworkingNavigation/ACLsNavigation/ExtendedACLs.ts
+++ src/Navigation/CoreNetworkingNavigation/ACLsNavigation/ExtendedACLs.ts
@@ -1,9 +1,20 @@
 import { Subpage } from "@/Navigation/NavigationTypes";
 
 const ExtendedACLs: Subpage = {
-  name: "Standard ACLs",
+  name: "Extended ACLs",
   subpages: [
-
+    {
+      name: "Extended ACL Basics",
+      path: "/corenetworking/security/acls/extended/basics",
+    },
+    {
+      name: "Extended ACL Syntax",
+      path: "/corenetworking/security/acls/extended/syntax",
+    },
+    {
+      name: "Extended ACL Placement",
+      path: "/corenetworking/security/acls/extended/placement",
+    },
   ],
 };
 
--- src/Navigation/CoreNetworkingNavigation/ACLsNavigation/VerifyingTroubleshooting.ts
+++ src/Navigation/CoreNetworkingNavigation/ACLsNavigation/VerifyingTroubleshooting.ts
@@ -3,10 +3,22 @@ import { Subpage } from "@/Navigation/NavigationTypes";
 const TroubleshootingACLs: Subpage = {
   name: "Verifying and Troubleshooting ACLs",
   subpages: [
-    { name: "show access-lists", subpages: [] },
-    { name: "show run / interface", subpages: [] },
-    { name: "ping and traceroute", subpages: [] },
-    { name: "Common Mistakes", subpages: [] },
+    {
+      name: "show access-lists",
+      path: "/corenetworking/security/acls/troubleshooting/showaccesslists",
+    },
+    {
+      name: "show run / interface",
+      path: "/corenetworking/security/acls/troubleshooting/showruninterface",
+    },
+    {
+      name: "ping and traceroute",
+      path: "/corenetworking/security/acls/troubleshooting/pingtraceroute",
+    },
+    {
+      name: "Common Mistakes",
+      path: "/corenetworking/security/acls/troubleshooting/commonmistakes",
+    },
   ],
 };
 
