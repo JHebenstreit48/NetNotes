2ca852b8cdddb79e95f4754c574c23605d9c64bc
Justin <j.hebenstreit25@gmail.com>
2025-08-09 21:58:13 -0500

    added new nav, routes, and pages for OSI

---
+++ src/Pages/MainTabs/CoreConcepts/Basics/OSIModel/Fundamentals/Overview.tsx
@@ -0,0 +1,18 @@
+import PageLayout from '@/Components/NavigationUI/PageLayout';
+import Header from '@/Components/Shared/Header/Header';
+import Notes from '@/Components/PageComponents/Notes/Notes';
+
+const Overview = () => {
+  const markdownFilePath = '/CoreConcepts/Basics/OSIModel/Fundamentals/Overview';
+
+  return (
+    <>
+      <PageLayout>
+        <Header text="Overview" />
+        <Notes filePath={markdownFilePath} />
+      </PageLayout>
+    </>
+  );
+};
+
+export default Overview;
