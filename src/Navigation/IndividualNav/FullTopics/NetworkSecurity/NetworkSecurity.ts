d2b2995a03c288714d357c97b330e06025cd3fe4
Justin <j.hebenstreit25@gmail.com>
2025-09-17 20:45:06 -0500

    fixed deployment issue

--- src/Navigation/IndividualNav/FullTopics/NetworkSecurity/NetworkSecurity.ts
+++ src/Navigation/IndividualNav/FullTopics/NetworkSecurity/NetworkSecurity.ts
@@ -1,13 +1,8 @@
 import { Subpage } from '@/Navigation/CombinedNav/CombinedNavAndTypes/NavigationTypes';
 
-import CoreConcepts from '@/Navigation/IndividualNav/Granularized/NetworkSecurity/NetworkSecurity/CoreConcepts';
-import PerimeterProtection from '@/Navigation/IndividualNav/Granularized/NetworkSecurity/NetworkSecurity/PerimeterProtection';
-
 const NetworkSecurity: Subpage = {
   name: 'Network Security',
   subpages: [
-    CoreConcepts,
-    PerimeterProtection
   ]
 };
 
