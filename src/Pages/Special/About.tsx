db0757cb0202bda8284494591a55f42620c8a534
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2026-01-24 17:58:15 -0600

    updated Home and About and other files

--- src/Pages/Special/About.tsx
+++ src/Pages/Special/About.tsx
@@ -1,23 +1,14 @@
-import { lazy, Suspense, useMemo } from "react";
 import Header from "@/Components/Shared/Header/Header";
-import aboutMd from "@/content/About.md?raw";
+import AboutContent from "@/Components/HomeAndAbout/aboutContent";
 import "@/scss/Page/Home/Home.scss";
 
-const HomeRenderer = lazy(
-  () => import("@/Components/PageComponents/Notes/HomeText/HomeRenderer")
-);
-
 export default function About() {
-  const content = useMemo(() => aboutMd, []);
-
   return (
     <>
       <Header />
       <main className="homePage">
         <div className="siteInfo">
-          <Suspense fallback={<p>Loading about content...</p>}>
-            <HomeRenderer content={content} />
-          </Suspense>
+          <AboutContent />
         </div>
       </main>
     </>
