b6d68590ad844d9039801f0be5a9ba02c2f036ba
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2026-02-25 15:37:13 -0600

    finished up pages and routing for new Coaxial page

--- src/routes/Individual/Granularized/Networking/TCPIPModel/Layers/NetworkAccess/Basics/Physical/CablingAndInterfaces.tsx
+++ src/routes/Individual/Granularized/Networking/TCPIPModel/Layers/NetworkAccess/Basics/Physical/CablingAndInterfaces.tsx
@@ -2,6 +2,7 @@ import { lazy } from 'react';
 import { RouteObject } from 'react-router-dom';
 
 const EthernetCablingCategories = lazy(() => import('@/Pages/MainTabs/Networking/TCPIPModel/Layers/NetworkAccess/Basics/Physical/CablingAndInterfaces/EthernetCablingCategories'));
+const CoaxialCable = lazy(() => import('@/Pages/MainTabs/Networking/TCPIPModel/Layers/NetworkAccess/Basics/Physical/CablingAndInterfaces/CoaxialCable'));
 const CopperConnectors = lazy(() => import('@/Pages/MainTabs/Networking/TCPIPModel/Layers/NetworkAccess/Basics/Physical/CablingAndInterfaces/CopperConnectors'));
 const FiberConnectors = lazy(() => import('@/Pages/MainTabs/Networking/TCPIPModel/Layers/NetworkAccess/Basics/Physical/CablingAndInterfaces/FiberConnectors'));
 
@@ -9,6 +10,10 @@ const BandwidthAndThroughput: RouteObject[] = [
   {
     path: '/tcpip-model/layers/network-access/basics/physical/cabling-and-interfaces/ethernet-cabling-categories',
     element: <EthernetCablingCategories />,
+  },
+  {
+    path: '/tcpip-model/layers/network-access/basics/physical/cabling-and-interfaces/coaxial-cable',
+    element: <CoaxialCable />,
   },
   {
     path: '/tcpip-model/layers/network-access/basics/physical/cabling-and-interfaces/copper-connectors',
