e401ae7029433c23aa421172a824774e79659c0d
Justin Hebenstreit <j.hebenstreit25@gmail.com>
2026-07-29 22:17:03 -0500

    update imports: scss/shared/Header -> header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+
 import Header from '@/components/shared/Header/Header';
 import Footer from '@/components/shared/Footer';
 import RouteTracker from '@/components/shared/analytics/RouteTracker';
 import BackToTopGate from '@/components/shared/BackToTop/Gate';
 
 import '@/scss/shared/Page.scss';
-import '@/scss/shared/Header/index.scss';
+import '@/scss/shared/header/index.scss';
 import '@/scss/shared/Footer.scss';
 import '@/scss/navigation/index.scss';
 import '@/scss/shared/Error.scss';
